0b77af9d431e7c5b6827c33c062fc4e8630283d8
mediapower13 <nurudeenbolaman@gmail.com>
2026-01-01 18:10:01 +0100

    expense

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
+    // Set to true during development if you want to ignore type errors
+    // Set to false for production to catch all type errors
     ignoreBuildErrors: false,
   },
 }
