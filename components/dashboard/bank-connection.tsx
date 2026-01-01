bd6aa3ba8660a4895a82921c7ebb0fca48410ab9
mediapower13 <nurudeenbolaman@gmail.com>
2026-01-01 18:07:36 +0100

    expense

--- components/dashboard/bank-connection.tsx
+++ components/dashboard/bank-connection.tsx
@@ -28,6 +28,7 @@ export function BankConnection() {
 
   useEffect(() => {
     loadBankAccounts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const loadBankAccounts = async () => {
